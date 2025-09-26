4e3f8e1c303fcb611304d09cd0d2ccad4e66e9eb
shadowwa <shadow.walker@free.fr>
2025-09-26 20:45:15 +0200

    rubocop fix

--- nerdfontslist.rb
+++ nerdfontslist.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'open-uri'
 require 'json'
 
 File.open('nerdfonts.txt', 'w') do |out|
-  URI.open(ARGV[1] || 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json') do |f|
+  URI.parse(ARGV[1] || 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json').open do |f|
     JSON.parse(f.read).each do |(name, subkey)|
       next unless subkey['char']
+
       out.puts "nf-#{name}:#{subkey['char']}"
     end
   end
